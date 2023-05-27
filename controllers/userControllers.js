028cf227cee85c3d75bb6c2541db3f4fbb39b922
Cheulong Sear <searcheulong@gmail.com>
2023-05-28 00:08:56 +0700

    integrate firestore

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,38 +1,66 @@
 import asyncHandler from "express-async-handler";
 import admin from "firebase-admin";
-import Todo from "../models/todo.model.js";
+import User from "../models/user.model.js";
 
-const getUsers = async (req, res) => {
-  const todosRef = await admin.firestore().collection("todos");
-  const data = await todosRef.get();
-  const todosArray = [];
-  data.forEach((doc) => {
-    const todo = new Todo(doc.id, doc.data().label);
-    todosArray.push(todo);
-  });
-  res.status(200).send(todosArray);
-};
+const getUsers = asyncHandler(async (req, res) => {
+  const usersRef = await admin.firestore().collection("users");
+  const data = await usersRef.get();
+  const usersArray = [];
+  if (data.empty) {
+    res.status(404).send("User record not found");
+  } else {
+    data.forEach((doc) => {
+      const user = new User(
+        doc.id,
+        doc.data().firstName,
+        doc.data().lastName,
+        doc.data().age
+      );
+      usersArray.push(user);
+    });
+    res.status(200).send(usersArray);
+  }
+});
 
-const getUser = async (req, res) => {
-  res.status(200).json({ message: `Get user with ${req.params.id}` });
-};
+const getUser = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const user = await admin.firestore().collection("users").doc(id);
+  const data = await user.get();
+  if (!data.exists) {
+    res.status(404).send("User with given ID not found");
+  } else {
+    res.status(200).json(data.data());
+  }
+});
 
 const createUser = asyncHandler(async (req, res, next) => {
-  console.log("The request body is ", req.body);
-  const { email, phone, name } = req.body;
-  if (!email || !phone || !name) {
+  const { firstName, lastName, age } = req.body;
+  if (!firstName || !lastName || !age) {
     res.status(400);
     throw new Error("All fields are required");
   }
-  res.status(201).json({ message: "Create user" });
+  const data = req.body;
+  await admin.firestore().collection("users").doc().set(data);
+  res.status(201).send("User created successfully");
 });
 
-const updateUser = async (req, res) => {
-  res.status(200).json({ message: `Update user with ${req.params.id}` });
-};
+const updateUser = asyncHandler(async (req, res) => {
+  const { firstName, lastName, age } = req.body;
+  if (!firstName || !lastName || !age) {
+    res.status(400);
+    throw new Error("All fields are required");
+  }
+  const id = req.params.id;
+  const data = req.body;
+  const user = await admin.firestore().collection("users").doc(id);
+  await user.update(data);
+  res.status(200).send("User updated successfully");
+});
 
-const deleteUser = async (req, res) => {
-  res.status(200).json({ message: `Delete user with ${req.params.id}` });
-};
+const deleteUser = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  await admin.firestore().collection("users").doc(id).delete();
+  res.status(200).send(`Delete user with ${req.params.id}`);
+});
 
 export { getUsers, getUser, createUser, updateUser, deleteUser };
--- models/todo.model.js
+++
@@ -1,8 +0,0 @@
-class Todo {
-  constructor(id, label) {
-    this.id = id;
-    this.label = label;
-  }
-}
-
-export default Todo;
